2aa874fa7b19fcd918a972dce5dcc4f77c7499fd
German Velasquez <72898005+gmcdreamy@users.noreply.github.com>
2021-12-06 20:02:11 -0600

    Create planet.ts

---
+++ examen german/app/service/planet.ts
@@ -0,0 +1,12 @@
+export class Planets{
+    name:string= "";
+    rotation_period:string="";
+    orbital_period:string="";
+    diameter:string="";
+    climate:string="";
+    gravity:string="";
+    terrain:string="";
+    surface_water:string="";
+    population:string="";
+}
+
